f0ed739fd3a1a0500070fc367ce209ab488ba49b
samwel <samwuelkinuthia@gmail.com>
2017-11-03 15:25:53 +0300

    set after_sign path to the dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  
+  def after_sign_in_path_for(_resource)
+    app_dashboard_index_path
+  end
 end
