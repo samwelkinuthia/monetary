47b8c17b85bbfe83f18c2ea180f2a4a6f662d005
samwel <samwuelkinuthia@gmail.com>
2017-11-03 15:46:51 +0300

    resourced deposits for easier routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'dashboard/index'
-
   devise_for :users, :controllers => { registrations: 'registrations' }
-
+  get 'dashboard/index'
   root'home#index'
+  resources :deposits
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
