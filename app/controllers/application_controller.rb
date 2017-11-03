a1e516158e5599d17be5cd1c00215f24bb0c1560
samwel <samwuelkinuthia@gmail.com>
2017-11-03 20:08:11 +0300

    redirect to dashboard after sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,4 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(_user)
     dashboard_index_path
   end
-  
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,7 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(_user)
-    edit_user_registration_path
+    dashboard_index_path
   end
 
   private
