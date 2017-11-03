edede9517fceb0f6fcadd9b7ac3c748ac44dd023
samwel <samwuelkinuthia@gmail.com>
2017-11-03 22:12:12 +0300

    permitted params for transfers

--- app/controllers/transfers_controller.rb
+++ app/controllers/transfers_controller.rb
@@ -2,4 +2,10 @@ class TransfersController < ApplicationController
   def index
     @transfer = Transfer.new
   end
+
+
+  private
+  def transfer_params
+    params.require(:transfer).permit(:amount, :recipient, :transfer_channel)
+  end
 end
