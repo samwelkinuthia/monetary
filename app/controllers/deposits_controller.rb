9b472c06d8be15e87afd3e2fe2cf5af82adb8972
samwel <samwuelkinuthia@gmail.com>
2017-11-03 16:20:11 +0300

    create method for deposit

--- app/controllers/deposits_controller.rb
+++ app/controllers/deposits_controller.rb
@@ -2,4 +2,12 @@ class DepositsController < ApplicationController
   def index
     @deposit = Deposit.new
   end
+
+  def create
+    @deposit = current_user.deposits.new(deposit_params)
+    if @deposit.save
+      redirect_to dashboard_path, notice: "Successfully deposited funds"
+    else
+      redirect_to dashboard_path, notice: "Deposit failed, try again"
+  end
 end
