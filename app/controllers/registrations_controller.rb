dde48d3a6553ff1d14d1d9a7d6138f981a1a5a9d
samwel <samwuelkinuthia@gmail.com>
2017-11-03 14:55:59 +0300

    private method sign_up for user sign up parameters

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,2 +1,10 @@
 class RegistrationsController < Devise::RegistrationsController
+
+private
+
+def sign_up_params
+  params.require(:user).permit(:email, :password, :password_confirmation)
+end
+
+
 end
