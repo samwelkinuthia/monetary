1d361fdbe7c59ab4d79745308760d976b5859d5d
samwel <samwuelkinuthia@gmail.com>
2017-11-03 21:52:29 +0300

    migrated table transfers into database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171103125123) do
+ActiveRecord::Schema.define(version: 20171103185122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,16 @@ ActiveRecord::Schema.define(version: 20171103125123) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_deposits_on_user_id"
+  end
+
+  create_table "transfers", force: :cascade do |t|
+    t.integer "amount"
+    t.integer "sender_id"
+    t.integer "recipient_id"
+    t.string "recipient"
+    t.string "transfer_channel"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
