54c6cf76a317841d0bc02a195aaee459c4626bd5
samwel <samwuelkinuthia@gmail.com>
2017-11-03 16:52:05 +0300

    dependent destoroy for deposits

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-
-  has_many :deposits
+  has_many :deposits, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
