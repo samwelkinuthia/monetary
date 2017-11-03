052efcb02ce3918716c100fe4f1f390bd6c1d2e7
samwel <samwuelkinuthia@gmail.com>
2017-11-03 15:51:50 +0300

    create model deposit, belongs to user, has amount

--- db/migrate/20171103125123_create_deposits.rb
+++ db/migrate/20171103125123_create_deposits.rb
@@ -1,6 +1,7 @@
 class CreateDeposits < ActiveRecord::Migration[5.1]
   def change
     create_table :deposits do |t|
+      t.integer :amount
       t.belongs_to :user, foreign_key: true
 
       t.timestamps
