a57921b9a1c98c41ddf43e5f79d853d528e1c31c
samwel <samwuelkinuthia@gmail.com>
2017-11-04 11:26:46 +0300

    method email for user and transfer

--- app/mailers/transfer_mailer.rb
+++ app/mailers/transfer_mailer.rb
@@ -1,2 +1,9 @@
 class TransferMailer < ApplicationMailer
+  default from: "monetary@gmail.com"
+
+  def transfer_email(user, transfer)
+    @user = user
+    @transfer = transfer
+    mail to: user.email, subjectL "Transfer confirmed"
+  end
 end
